f399a2cb4ef9968e43c879277fce57aa0be81159
oliespineira <oliviaen06@gmail.com>
2026-09-22 20:58:53 +0200

    part 2

--- campaign_launchpad/budget.py
+++ campaign_launchpad/budget.py
@@ -5,13 +5,24 @@ class GlobalBudget:
     """
     _instance = None
 
+
     def __new__(cls, initial_amount: float = 0.0):
       # TODO: Singleton pattern implementation
-      pass
+      if cls._instance is None:
+         instance = super().__new__(cls) #interesting, so you don´t need to hardcode the object directly (could use object, that's the root class every other class in Python ultimately derives from)
+         instance._balance = initial_amount
+         cls._instance = instance
+      return cls._instance
+    #now the class atribute no longer points to None but to the actual GlobalBudget instance.
+      
 
     def allocate(self, amount: float) -> None:
       # TODO: Allocate amount from the budget
-      pass
+      if amount <= 0:
+         raise ValueError("Allocation amount must be larger thn 0.")
+      if amount > self._balance:
+          raise ValueError("Insufficient funds in the budget.")
+      self._balance -= amount
 
     def remaining(self) -> float:
         return self._balance
