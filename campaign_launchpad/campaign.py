3daf617bfaa4763ae41f661a87701d2471c2a069
oliespineira <oliviaen06@gmail.com>
2026-09-22 12:16:14 +0200

    constructor and setters for campaign

--- campaign_launchpad/campaign.py
+++ campaign_launchpad/campaign.py
@@ -18,37 +18,80 @@ class Campaign:
 
 class CampaignBuilder:
     def __init__(self):
-      # TODO
-      pass
+      # TODO: 
+      self._name = None
+      self._channel = None
+      self._daily_budget = None
+      self._start_date = None
+      self._end_date = None
+      self._target_audience = {}
+      self._creatives = []
+      self._tracking = {}
 
-    def with_name(self, name: str):
+
+
+
+    def with_name(self, name: str): #setters
       # TODO
-      pass
+      self.name = name
+      return self
+
 
     def with_channel(self, channel: str):
       # TODO
-      pass
+      self._channel= channel
+      return self
 
     def with_budget(self, daily_budget: float):
       # TODO
-      pass
+      if daily_budget <= 0:
+        raise ValueError("Daily budget cannot be negative.")
+      else:
+        self._daily_budget = daily_budget
+      return self
 
     def with_dates(self, start_date, end_date=None):
       # TODO
-      pass
+      if end_date is not None and end_date < start_date:
+            raise ValueError("End date cannot be earlier than start date.")
+      self._start_date = start_date
+      self._end_date = end_date
+      return self
 
     def with_audience(self, **kwargs):
-      # TODO
-      pass
+      self._target_audience.update(kwargs)
+
+      return self
 
     def add_creative(self, headline: str, image_url: str):
-      # TODO
-      pass
+      self._creatives.append({"headline": headline, "image_url": image_url})
+      return self
 
     def with_tracking(self, **kwargs):
-      # TODO
-      pass
+      self._tracking.update(kwargs)
+      return self
 
     def build(self) -> Campaign:
       # TODO: Validations and return Campaign instance
-      pass
+      if not self._name:
+        raise ValueError("Campaign must have a name.")
+      if not self._channel:
+        raise ValueError("Campaign channel cannot be empty.")
+      if not self._daily_budget or self._daily_budget <= 0:
+        raise ValueError("Daily budget must exist and be a positive number.")
+      if not self._start_date:
+        raise ValueError("Start date must be provided.")
+      if not self._creatives:
+        raise ValueError("At least one creative must be provided.")
+      
+      
+      return Campaign(
+          name=self._name,
+          channel=self._channel,
+          daily_budget=self._daily_budget,
+          start_date=self._start_date,
+          end_date=self._end_date,
+          target_audience=self._target_audience,
+          creatives=self._creatives,
+          tracking=self._tracking,
+      )
