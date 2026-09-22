6f6d9e2a90f1bb573b533ce8b7c983a6010eac89
oliespineira <oliviaen06@gmail.com>
2026-09-22 22:03:25 +0200

    third exercise completed (channels)

--- campaign_launchpad/channels.py
+++ campaign_launchpad/channels.py
@@ -21,14 +21,33 @@ class ChannelClient(ABC):
 
 class GoogleAdsClient(ChannelClient):
   # TODO: Implement the Google Ads specific logic here.
-  pass
+
+  def create_campaign(self, campaign:Campaign) -> str:
+     GlobalBudget().allocate(campaign.daily_budget)
+     return f"g-{uuid4()}"
+
+  def pause_campaign(self, campaign_id:str) -> None:
+     
+    pass
 
 class FacebookAdsClient(ChannelClient):
   # TODO: Implement the Facebook Ads specific logic here.
-  pass
+
+
+    def create_campaign(self, campaign: Campaign) -> str:
+        GlobalBudget().allocate(campaign.daily_budget)
+        return f"f-{uuid4()}"
+
+    def pause_campaign(self, campaign_id: str) -> None:
+        pass
 
 class ChannelClientFactory:
     @staticmethod
     def create(channel: str) -> ChannelClient:
       # TODO: Return the appropriate client based on the channel.
-      pass
+      if channel == "google":
+         return GoogleAdsClient("Google Ads")
+      elif channel == "facebook":
+         return FacebookAdsClient("Facebook Ads")
+      else:
+         raise ValueError(f"Unsupported channel: {channel}")
